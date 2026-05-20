d660d5a2a0fc3a71b88e0ce85c82313e2768c970
KarinaCupolaAlves <karina.alves@sptech.school>
2026-05-20 14:35:36 -0300

    fix: senha passada na permissao da criação do usuario

--- database/script_usuario.sql
+++ database/script_usuario.sql
@@ -1,7 +1,7 @@
 show databases;
 
 CREATE USER 'user_insert'@'%' IDENTIFIED BY 'SPTech#2024';
-GRANT INSERT ON flowtrak.dado_captado TO 'user_insert'@'SPTech#2024';
+GRANT INSERT ON flowtrak.dado_captado TO 'user_insert'@'%';
 
 
 CREATE USER 'user_admin'@'%' IDENTIFIED BY 'SPTech#2024';
